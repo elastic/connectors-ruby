3b686e3353b5dda1e850b3442f92afe22fd4ba5d
Sean Story <sean.j.story@gmail.com>
2022-01-06 14:37:22 -0600

    Add license headers

--- hello-world-connector/src/main/ruby/hello_world.rb
+++ hello-world-connector/src/main/ruby/hello_world.rb
@@ -1,3 +1,9 @@
+#
+# Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+# or more contributor license agreements. Licensed under the Elastic License;
+# you may not use this file except in compliance with the Elastic License.
+#
+
 require 'java'
 require 'awesome_print'
 java_import 'co.elastic.connectors.api.Connector'
