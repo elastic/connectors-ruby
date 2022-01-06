f3bd4d655d46a62f0f429afa442422b793e31bae
Sean Story <sean.j.story@gmail.com>
2022-01-06 14:55:42 -0600

    Added CI

---
+++ .ci/pipelines/connectors.groovy
@@ -0,0 +1,29 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+// Loading the shared lib
+@Library(['estc', 'entsearch']) _
+
+eshPipeline(
+    timeout: 45,
+    project_name: 'Connectors',
+    repository: 'connectors',
+    stage_name: 'Connectors Unit Tests',
+    stages: [
+        [
+            name: 'Maven Build',
+            type: 'script',
+            label: 'Maven Build',
+            script: {
+                withMaven {
+                    sh './mvnw clean verify'
+                }
+            },
+            match_on_all_branches: true,
+        ]
+    ],
+    slack_channel: 'workplace-search-connectors'
+)
