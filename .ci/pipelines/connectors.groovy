9330f6dd288d01505818d4bfb0fcacfd220dedc9
Artem Shelkovnikov <artem.shelkovnikov@elastic.co>
2022-02-18 13:55:01 +0100

    What's up with the syntax?

--- .ci/pipelines/connectors.groovy
+++ .ci/pipelines/connectors.groovy
@@ -12,23 +12,25 @@ eshPipeline(
     project_name: 'Connectors',
     repository: 'connectors',
     stage_name: 'Connectors Unit Tests',
-    [
-        name: 'Tests',
-        type: 'script',
-        label: 'Makefile',
-        script: {
-            sh 'make test'
-        },
-        match_on_all_branches: true,
-    ],
-    [
-        name: 'Packaging',
-        type: 'script',
-        label: 'Makefile',
-        script: {
-            sh 'make build'
-        },
-        match_on_all_branches: true,
+    stages: [
+        [
+            name: 'Tests',
+            type: 'script',
+            label: 'Makefile',
+            script: {
+                sh 'make test'
+            },
+            match_on_all_branches: true,
+        ],
+        [
+            name: 'Packaging',
+            type: 'script',
+            label: 'Makefile',
+            script: {
+                sh 'make build'
+            },
+            match_on_all_branches: true,
+        ]
     ],
     slack_channel: 'workplace-search-connectors'
 )
