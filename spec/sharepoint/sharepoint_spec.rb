07abccbce7c24096fa1f68d91fee80a090d85d87
Tarek Ziade <tarek@ziade.org>
2022-02-22 10:34:32 +0100

    cleanup

--- spec/sharepoint/sharepoint_spec.rb
+++ spec/sharepoint/sharepoint_spec.rb
@@ -33,70 +33,37 @@ RSpec.describe Sharepoint::HttpCallWrapper do
     described_class.new(content_source, Config.new)
   end
 
+  def mock_endpoint(path, data)
+    data = JSON.generate(data)
+
+    stub_request(:get, "https://graph.microsoft.com/v1.0/#{path}")
+      .with { true }
+      .to_return(status: 200, body: data)
+  end
+
   it 'can get documents' do
-    sites = JSON.generate({ value: [{ id: 4567 }] })
-
-    stub_request(:get, 'https://graph.microsoft.com/v1.0/sites/?$select=id&search=&top=10')
-      .with { true }
-      .to_return(status: 200, body: sites, headers: {})
-
+    # fake data
+    sites = { value: [{ id: 4567 }] }
     groups = { value: [{ id: 1234, createdDateTime: Time.now }] }
+    drives = { value: [{ id: 4567, driveType: 'documentLibrary' }] }
+    drive = { id: 1111, driveType: 'documentLibrary' }
+    children = { value: [{ folder: 'folder', id: 1111,
+                           name: 'item' }] }
+    permissions = {
+      value: [
+        { id: 666 }
+      ]
+    }
 
-    stub_request(:get, 'https://graph.microsoft.com/v1.0/groups/?$select=id,createdDateTime')
-      .with { true }
-      .to_return(status: 200, body: JSON.generate(groups), headers: {})
-
-    stub_request(:get, 'https://graph.microsoft.com/v1.0/groups/1234/sites/root?$select=id')
-      .with { true }
-      .to_return(status: 200,
-                 body: sites,
-                 headers: {})
-
-    drives = JSON.generate({
-                             value: [{ id: 4567,
-                                       driveType: 'documentLibrary' }]
-                           })
-
-    stub_request(:get, 'https://graph.microsoft.com/v1.0/sites/4567/drives/?$select=id,owner,name,driveType')
-      .with { true }
-      .to_return(status: 200,
-                 body: drives,
-                 headers: {})
+    mock_endpoint('sites/?$select=id&search=&top=10', sites)
+    mock_endpoint('groups/?$select=id,createdDateTime', groups)
+    mock_endpoint('groups/1234/sites/root?$select=id', sites)
+    mock_endpoint('sites/4567/drives/?$select=id,owner,name,driveType', drives)
     # ??
-    stub_request(:get, 'https://graph.microsoft.com/v1.0/sites//drives/?$select=id,owner,name,driveType')
-      .with { true }
-      .to_return(status: 200,
-                 body: drives,
-                 headers: {})
-
-    drive = JSON.generate({ id: 1111,
-                            driveType: 'documentLibrary' })
-
-    stub_request(:get, 'https://graph.microsoft.com/v1.0/drives/4567/root?$select=id')
-      .with { true }
-      .to_return(status: 200,
-                 body: drive,
-                 headers: {})
-
-    children = JSON.generate({ value: [{ folder: 'folder', id: 1111,
-                                         name: 'item' }] })
-
-    stub_request(:get, 'https://graph.microsoft.com/v1.0/drives/4567/items/1111/children')
-      .with { true }
-      .to_return(status: 200,
-                 body: children,
-                 headers: {})
-
-    permissions = JSON.generate({
-                                  value: [
-                                    { id: 666 }
-                                  ]
-                                })
-    stub_request(:get, 'https://graph.microsoft.com/v1.0/drives/4567/items/1111/permissions')
-      .with { true }
-      .to_return(status: 200,
-                 body: permissions,
-                 headers: {})
+    mock_endpoint('sites//drives/?$select=id,owner,name,driveType', drives)
+    mock_endpoint('drives/4567/root?$select=id', drive)
+    mock_endpoint('drives/4567/items/1111/children', children)
+    mock_endpoint('drives/4567/items/1111/permissions', permissions)
 
     expect(backend.get_document_batch.size).to eq 11
   end
