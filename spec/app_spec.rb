6667cf113022792d2943f42d54cf8af50497eb08
Tarek Ziadé <tarek@ziade.org>
2022-03-01 13:26:09 +0100

    forgot the spec

---
+++ spec/app_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'app/app'
+
+ENV['APP_ENV'] = 'test'
+
+RSpec.describe ConnectorsWebApp do
+  include Rack::Test::Methods
+
+  let(:app) { ConnectorsWebApp }
+
+  context 'GET to /status' do
+    let(:response) { get '/status' }
+
+    it 'returns status 200 OK' do
+      stub_request(:get, 'https://graph.microsoft.com/v1.0/me')
+        .with { true }
+        .to_return(status: 200, body: JSON.generate({}))
+
+      puts(response.body)
+      expect(response.status).to eq 200
+    end
+  end
+end
