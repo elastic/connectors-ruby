cb297e99dd3ab7837608427ae2eaa72efb0d6a25
Chenhui Wang <wangch079@outlook.com>
2022-03-03 16:31:33 +0800

    OAuth tests

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -22,6 +22,34 @@ RSpec.describe ConnectorsWebApp do
     end
   end
 
+  describe 'POST /oauth2/init' do
+    context 'with valid request' do
+      let(:params) { { :client_id => 'client id' } }
+      let(:authorization_uri) { 'authorization_uri' }
+
+      it 'returns authorization uri' do
+        allow(Connectors::Sharepoint::Authorization).to receive(:authorization_uri).and_return(authorization_uri)
+        response = post('/oauth2/init', JSON.generate(params), { 'CONTENT_TYPE' => 'application/json' })
+        expect(response).to be_successful
+        response_json = JSON.parse(response.body)
+        expect(response_json['oauth2redirect']).to eq(authorization_uri)
+      end
+    end
+
+    context 'with invalid request' do
+      let(:params) { {} }
+      let(:error) { 'error' }
+
+      it 'returns bad request' do
+        allow(Connectors::Sharepoint::Authorization).to receive(:authorization_uri).and_raise(ConnectorsShared::ClientError.new(error))
+        response = post('/oauth2/init', JSON.generate(params), { 'CONTENT_TYPE' => 'application/json' })
+        expect(response.status).to eq(400)
+        response_json = JSON.parse(response.body)
+        expect(response_json['errors'].first['message']).to eq(error)
+      end
+    end
+  end
+
   describe 'Oauth2 dance' do
     it 'does the oauth2 dance' do
       # we call /oauth2/init with the client_id and client_secret
