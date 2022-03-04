00f2fd8c949e4c19481d8f8abe65744b13a4bda2
Tarek Ziade <tarek@ziade.org>
2022-03-04 15:08:34 +0100

    license header

--- lib/app/app.rb
+++ lib/app/app.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
+#
+# Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+# or more contributor license agreements. Licensed under the Elastic License;
+# you may not use this file except in compliance with the Elastic License.
+#
 
 require 'faraday'
 require 'hashie'
