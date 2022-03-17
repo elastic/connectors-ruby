1a1632c3059e2fec2fa1c21e5e73138310b9ab6f
Ross Bell <rossallenbell@gmail.com>
2022-03-17 11:54:18 -0500

    Readme and comment

--- lib/app/console.rb
+++ lib/app/console.rb
@@ -1,3 +1,10 @@
+# frozen_string_literal: true
+#
+# Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+# or more contributor license agreements. Licensed under the Elastic License;
+# you may not use this file except in compliance with the Elastic License.
+#
+
 $LOAD_PATH << '../'
 
 require './app'
