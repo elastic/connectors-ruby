23873bd8dffa5e88eac77d465ccefef44e414766
Sean Story <sean.j.story@gmail.com>
2022-03-17 15:02:22 -0500

    Added spec to check for license and notice concerns

--- lib/app/console.rb
+++ lib/app/console.rb
@@ -1,9 +1,10 @@
-# frozen_string_literal: true
 #
 # Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
 # or more contributor license agreements. Licensed under the Elastic License;
 # you may not use this file except in compliance with the Elastic License.
 #
+
+# frozen_string_literal: true
 
 $LOAD_PATH << '../'
 
--- lib/connectors_shared.rb
+++ lib/connectors_shared.rb
@@ -1,3 +1,9 @@
+#
+# Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+# or more contributor license agreements. Licensed under the Elastic License;
+# you may not use this file except in compliance with the Elastic License.
+#
+
 require 'connectors_shared/constants'
 require 'connectors_shared/errors'
 require 'connectors_shared/exception_tracking'
