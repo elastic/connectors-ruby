03efee3d479d50e8a001415b4a1fba9c75a254e8
Chenhui Wang <wangch079@outlook.com>
2022-04-28 15:33:29 +0800

    catch third party errors

--- lib/connectors_sdk/base/http_call_wrapper.rb
+++ lib/connectors_sdk/base/http_call_wrapper.rb
@@ -23,49 +23,49 @@ module ConnectorsSdk
       end
 
       def document_batch(params)
-        results = []
+        convert_third_party_errors do
+          results = []
 
-        extractor = extractor(params)
+          extractor = extractor(params)
 
-        extractor.yield_document_changes(:break_after_page => true, :modified_since => extractor.config.cursors['modified_since']) do |action, doc, download_args_and_proc|
-          download_obj = nil
-          if download_args_and_proc
-            download_obj = {
-              id: download_args_and_proc[0],
-              name: download_args_and_proc[1],
-              size: download_args_and_proc[2],
-              download_args: download_args_and_proc[3]
+          extractor.yield_document_changes(:break_after_page => true, :modified_since => extractor.config.cursors['modified_since']) do |action, doc, download_args_and_proc|
+            download_obj = nil
+            if download_args_and_proc
+              download_obj = {
+                  id: download_args_and_proc[0],
+                  name: download_args_and_proc[1],
+                  size: download_args_and_proc[2],
+                  download_args: download_args_and_proc[3]
+              }
+            end
+
+            results << {
+                :action => action,
+                :document => doc,
+                :download => download_obj
             }
           end
 
-          results << {
-            :action => action,
-            :document => doc,
-            :download => download_obj
-          }
+          [results, extractor.config.cursors, extractor.completed]
         end
-
-        [results, extractor.config.cursors, extractor.completed]
-      rescue custom_client_error => e
-        raise e.status_code == 401 ? ConnectorsShared::InvalidTokenError : e
       end
 
       def deleted(params)
-        results = []
-        extractor(params).yield_deleted_ids(params[:ids]) do |id|
-          results << id
+        convert_third_party_errors do
+          results = []
+          extractor(params).yield_deleted_ids(params[:ids]) do |id|
+            results << id
+          end
+          results
         end
-        results
-      rescue custom_client_error => e
-        raise e.status_code == 401 ? ConnectorsShared::InvalidTokenError : e
       end
 
       def permissions(params)
-        extractor(params).yield_permissions(params[:user_id]) do |permissions|
-          return permissions
+        convert_third_party_errors do
+          extractor(params).yield_permissions(params[:user_id]) do |permissions|
+            return permissions
+          end
         end
-      rescue custom_client_error => e
-        raise e.status_code == 401 ? ConnectorsShared::InvalidTokenError : e
       end
 
       def authorization_uri(params)
@@ -100,6 +100,12 @@ module ConnectorsSdk
       end
 
       private
+
+      def convert_third_party_errors
+        yield
+      rescue custom_client_error => e
+        raise e.status_code == 401 ? ConnectorsShared::InvalidTokenError : e
+      end
 
       def extractor_class
         raise 'Not implemented for this connector'
