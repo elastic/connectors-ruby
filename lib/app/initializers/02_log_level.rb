e923ad8d6a54b1994d2f48b98f32ec837c78040a
Artem Shelkovnikov <artem.shelkovnikov@elastic.co>
2022-05-23 10:27:54 +0200

    Add a license header

--- lib/app/initializers/02_log_level.rb
+++ lib/app/initializers/02_log_level.rb
@@ -1,3 +1,10 @@
+# frozen_string_literal: true
+#
+# Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+# or more contributor license agreements. Licensed under the Elastic License;
+# you may not use this file except in compliance with the Elastic License.
+#
+
 logger = AppConfig.connectors_logger
 logger.level = ConnectorsApp::Config['log_level'] || 'info'
 
