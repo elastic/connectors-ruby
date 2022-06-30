5632cafb9add8433f546b20236f01399c1803293
Maryna Cherniavska <maryna.cherniavska@elastic.co>
2022-06-30 16:51:47 +0200

    fixing tests and lint

--- spec/connectors/gitlab/connector_spec.rb
+++ spec/connectors/gitlab/connector_spec.rb
@@ -7,8 +7,13 @@ require 'connectors/gitlab/custom_client'
 describe Connectors::GitLab::Connector do
   let(:user_json) { connectors_fixture_raw('gitlab/user.json') }
   let(:base_url) { Connectors::GitLab::DEFAULT_BASE_URL }
+  let(:app_config) { Hashie::Mash.new(:gitlab => { :base_url => base_url, :api_token => 'some_token' }) }
+
 
   context '#source_status' do
+    before do
+      stub_const('App::Config', app_config)
+    end
     it 'correctly returns true on 200' do
       stub_request(:get, "#{base_url}/user")
         .to_return(:status => 200, :body => user_json)
