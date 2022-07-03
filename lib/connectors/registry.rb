1c7294caacfa7d88d305a0c4e494309fcdeb3285
Artem Shelkovnikov <artem.shelkovnikov@elastic.co>
2022-07-03 16:06:55 +0200

    Fix other stuff

--- lib/connectors/registry.rb
+++ lib/connectors/registry.rb
@@ -5,43 +5,41 @@
 #
 
 module Connectors
-  module Base
-    class Factory
-      attr_reader :connectors
+  class Factory
+    attr_reader :connectors
 
-      def initialize
-        @connectors = {}
-      end
-
-      def register(name, klass)
-        @connectors[name] = klass
-      end
-
-      def connector_class(name)
-        @connectors[name]
-      end
-
-      def connector(name, params = nil)
-        klass = connector_class(name)
-        if klass.present?
-          return params.nil? ? klass.new : klass.new(params)
-        end
-        raise "Connector #{name} is not yet registered. You need to register it before use"
-      end
-
-      def registered_connectors
-        @connectors.keys.sort
-      end
+    def initialize
+      @connectors = {}
     end
 
-    REGISTRY = Factory.new
+    def register(name, klass)
+      @connectors[name] = klass
+    end
 
-    require_relative '../stub_connector/connector'
-    REGISTRY.register(Connectors::StubConnector::Connector::SERVICE_TYPE, Connectors::StubConnector::Connector)
+    def connector_class(name)
+      @connectors[name]
+    end
 
-    # loading plugins (might replace this with a directory scan and conventions on names)
-    require_relative '../gitlab/connector'
+    def connector(name, params = nil)
+      klass = connector_class(name)
+      if klass.present?
+        return params.nil? ? klass.new : klass.new(params)
+      end
+      raise "Connector #{name} is not yet registered. You need to register it before use"
+    end
 
-    REGISTRY.register(Connectors::GitLab::Connector::SERVICE_TYPE, Connectors::GitLab::Connector)
+    def registered_connectors
+      @connectors.keys.sort
+    end
   end
+
+  REGISTRY = Factory.new
+
+  require_relative './stub_connector/connector'
+  REGISTRY.register(Connectors::StubConnector::Connector::SERVICE_TYPE, Connectors::StubConnector::Connector)
+
+  # loading plugins (might replace this with a directory scan and conventions on names)
+  require_relative './gitlab/connector'
+
+  REGISTRY.register(Connectors::GitLab::Connector::SERVICE_TYPE, Connectors::GitLab::Connector)
 end
