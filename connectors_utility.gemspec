4293176c8672d2ef42e2690c04c19b5b4e9b0d11
Dmitriy Burlutskiy <vidokX@gmail.com>
2022-07-05 15:57:46 +0200

    Default Elasticsearch index mapping (#131)
    Introduce an abstraction to generate Elasticsearch index settings and mappings needed to enrich full-text search.

---
+++ connectors_utility.gemspec
@@ -0,0 +1,18 @@
+Gem::Specification.new do |s|
+  s.name        = 'connectors_utility'
+  s.version     = '0.5'
+  s.homepage    = 'https://github.com/elastic/connectors'
+  s.summary     = 'Gem containing shared Connector Services libraries'
+  s.description = ''
+  s.authors     = ['Elastic']
+  s.email       = 'ent-search-dev@elastic.co'
+  s.files       = %w[
+                    LICENSE
+                    NOTICE.txt
+                    lib/connectors_utility.rb
+                    lib/utility/elasticsearch/index/text_analysis_settings.rb
+                    lib/utility/elasticsearch/index/mappings.rb
+                    lib/utility/elasticsearch/index/language_data.yml
+                  ]
+  s.license     = 'Elastic-2.0'
+end
---
+++ lib/utility/elasticsearch/index/mappings.rb
@@ -0,0 +1,78 @@
+#
+# Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+# or more contributor license agreements. Licensed under the Elastic License;
+# you may not use this file except in compliance with the Elastic License.
+#
+
+# frozen_string_literal: true
+
+module Utility
+  module Elasticsearch
+    module Index
+      module Mappings
+        ENUM_IGNORE_ABOVE = 2048
+
+        WORKPLACE_SEARCH_SUBEXTRACTION_STAMP_FIELD_MAPPINGS = {
+          _subextracted_as_of: {
+            type: 'date'
+          },
+          _subextracted_version: {
+            type: 'keyword'
+          }
+        }.freeze
+
+        def self.default_text_fields_mappings(search_index:)
+          {
+            dynamic: true,
+            dynamic_templates: [
+              {
+                data: {
+                  match_mapping_type: 'string',
+                  mapping: {
+                    type: 'text',
+                    analyzer: 'iq_text_base',
+                    index_options: 'freqs',
+                    fields: {
+                      'stem': {
+                        type: 'text',
+                        analyzer: 'iq_text_stem'
+                      },
+                      'prefix' => {
+                        type: 'text',
+                        analyzer: 'i_prefix',
+                        search_analyzer: 'q_prefix',
+                        index_options: 'docs'
+                      },
+                      'delimiter' => {
+                        type: 'text',
+                        analyzer: 'iq_text_delimiter',
+                        index_options: 'freqs'
+                      },
+                      'joined': {
+                        type: 'text',
+                        analyzer: 'i_text_bigram',
+                        search_analyzer: 'q_text_bigram',
+                        index_options: 'freqs'
+                      },
+                      'enum': {
+                        type: 'keyword',
+                        ignore_above: ENUM_IGNORE_ABOVE
+                      }
+                    }
+                  }
+                }
+              }
+            ],
+            properties: {
+              id: {
+                type: 'keyword'
+              }
+            }.tap do |properties|
+              properties.merge!(WORKPLACE_SEARCH_SUBEXTRACTION_STAMP_FIELD_MAPPINGS) if search_index
+            end
+          }
+        end
+      end
+    end
+  end
+end
