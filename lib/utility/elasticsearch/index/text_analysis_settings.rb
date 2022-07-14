a3a0b45f4fbd31d42a3e25fb102181ebe89e5093
Dmitriy Burlutskiy <dmitrii.burlutckii@elastic.co>
2022-07-14 14:11:02 +0200

    Unspecified language_code fallbacks to english (#179)

--- lib/utility/elasticsearch/index/text_analysis_settings.rb
+++ lib/utility/elasticsearch/index/text_analysis_settings.rb
@@ -63,8 +63,8 @@ module Utility
           tokenizer_name: 'icu_tokenizer', folding_filters: %w(icu_folding)
         }.freeze
 
-        def initialize(language_code: DEFAULT_LANGUAGE, analysis_icu: false)
-          @language_code = language_code.to_sym
+        def initialize(language_code: nil, analysis_icu: false)
+          @language_code = (language_code || DEFAULT_LANGUAGE).to_sym
 
           raise UnsupportedLanguageCode unless language_data[@language_code]
 
