3462d7561b1bc4a20dec2162b01eb09adf64f0b1
Artem Shelkovnikov <artem.shelkovnikov@elastic.co>
2022-07-27 14:07:17 +0200

    Linters

--- lib/core/heartbeat.rb
+++ lib/core/heartbeat.rb
@@ -23,11 +23,13 @@ module Core
           connector_class = Connectors::REGISTRY.connector_class(service_type)
           doc[:configuration] = connector_class.configurable_fields
 
+          # We want to set connector to CONFIGURED status if all configurable fields have default values
           new_connector_status = if configuration.values.all? { |setting| setting[:value] }
-            Connectors::ConnectorStatus::CONFIGURED
-          else
-            Connectors::ConnectorStatus::NEEDS_CONFIGURATION
-          end
+                               Connectors::ConnectorStatus::CONFIGURED
+                             else
+                               Connectors::ConnectorStatus::NEEDS_CONFIGURATION
+                             end
+
           doc[:status] = new_connector_status
         elsif connector_settings.connector_status_allows_sync?
           connector_instance = Connectors::REGISTRY.connector(service_type, connector_settings.configuration)
