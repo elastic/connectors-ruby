a68434c314ccda0a7b8f8adcb4f5b7e86f4700eb
Artem Shelkovnikov <lavatroublebubble@gmail.com>
2022-08-11 00:04:33 +0200

    Add tests for Core::Output_Sink::* + refactor sinks a bit (#254)
    (cherry picked from commit 4fb4d2211308a247382092855a46d757b1a9bf5c)

--- spec/support/shared_examples.rb
+++ spec/support/shared_examples.rb
@@ -6,6 +6,15 @@
 
 # frozen_string_literal: true
 
+shared_examples 'implements all methods of base class' do
+  it '' do
+    base_class_public_methods = get_class_specific_public_methods(base_class_instance)
+    specific_class_public_methods = get_class_specific_public_methods(concrete_class_instance)
+
+    expect(specific_class_public_methods).to eq(base_class_public_methods)
+  end
+end
+
 shared_examples 'does not populate updated_at' do
   it 'returns document that does not have updated_at field' do
     expect(document.with_indifferent_access).to_not include(have_key(:updated_at))
