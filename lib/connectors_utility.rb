b0dc102f65609a2060b3c7f35e90304ce3d356b3
Dmitrii Burlutckii <dmitrii.burlutckii@elastic.co>
2022-09-06 16:56:37 +0200

    Remove unnecessary file requirements

--- lib/connectors_utility.rb
+++ lib/connectors_utility.rb
@@ -6,20 +6,10 @@
 
 # frozen_string_literal: true
 
-require_relative 'utility/constants'
-require_relative 'utility/logger'
-require_relative 'utility/cron'
-require_relative 'utility/errors'
-require_relative 'utility/es_client'
-require_relative 'utility/environment'
-require_relative 'utility/exception_tracking'
-require_relative 'utility/extension_mapping_util'
-require_relative 'utility/elasticsearch/index/mappings'
-require_relative 'utility/elasticsearch/index/text_analysis_settings'
+require_relative 'utility'
 
 require_relative 'connectors/connector_status'
 require_relative 'connectors/sync_status'
 require_relative 'core/scheduler'
 require_relative 'core/crawler_scheduler'
 require_relative 'core/elastic_connector_actions'
-require_relative 'utility'
