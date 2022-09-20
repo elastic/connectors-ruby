de2d0b903fde61485c5fa2b8e4eff2a1339fc62d
Chenhui Wang <wangch079@outlook.com>
2022-09-20 18:11:42 +0800

    update specs

--- spec/utility/logger_spec.rb
+++ spec/utility/logger_spec.rb
@@ -15,11 +15,11 @@ RSpec.describe Utility::Logger do
   let(:message_with_breaks) { 'This is a message with line breaks.\nThis is a message with line breaks.' }
   let(:message_with_tabs) { 'This is a message  with tabs.\t\t\tThis is a message  with tabs.' }
   let(:message_with_many_spaces) { '  This is a    message with a lot of spaces.  ' }
-  let(:global_settings) { double(:ecs_logging => false) }
+  let(:ecs_logging) { false }
 
   before do
     stub_const('Utility::Logger::MAX_SHORT_MESSAGE_LENGTH', 100)
-    stub_const('Settings', global_settings)
+    allow_any_instance_of(::Config::Options).to receive(:ecs_logging).and_return(ecs_logging)
   end
 
   it 'can give the connectors logger' do
@@ -43,5 +43,20 @@ RSpec.describe Utility::Logger do
     expect(msg.match(/\s{2,}/)).to be_falsey
     expect(msg.match(/^\s/)).to be_falsey
     expect(msg.match(/\s$/)).to be_falsey
+  end
+
+  context 'with ecs logging' do
+    let(:ecs_logging) { true }
+    let(:version) { '8.5.0' }
+
+    before do
+      allow_any_instance_of(::Config::Options).to receive(:version).and_return(version)
+    end
+
+    it 'outputs ecs fields' do
+      expect { described_class.info(message) }.to output(/@timestamp/).to_stdout_from_any_process
+      expect { described_class.info(message) }.to output(/ecs\.version/).to_stdout_from_any_process
+      expect { described_class.info(message) }.to output(/#{version}/).to_stdout_from_any_process
+    end
   end
 end
