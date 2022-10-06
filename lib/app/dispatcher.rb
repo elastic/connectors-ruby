22097e45304120e666fbb9f951abc65e7ce5d0c7
Ubuntu <ubuntu@ip-172-31-36-220.us-east-2.compute.internal>
2022-10-06 20:43:02 +0000

    Another file

--- lib/app/dispatcher.rb
+++ lib/app/dispatcher.rb
@@ -7,7 +7,6 @@
 # frozen_string_literal: true
 
 require 'active_support'
-require 'concurrent'
 require 'connectors'
 require 'core'
 require 'utility'
@@ -22,39 +21,17 @@ module App
     MAX_THREADS = (App::Config.dig(:thread_pool, :max_threads) || 5).to_i
     MAX_QUEUE = (App::Config.dig(:thread_pool, :max_queue) || 100).to_i
 
-    @running = Concurrent::AtomicBoolean.new(false)
-
     class << self
       def start!
-        running!
         Utility::Logger.info("Starting connector service in #{App::Config.native_mode ? 'native' : 'non-native'} mode...")
         start_polling_jobs!
       end
 
       def shutdown!
-        Utility::Logger.info("Shutting down connector service with pool [#{pool.class}]...")
-        running.make_false
         scheduler.shutdown
-        pool.shutdown
-        pool.wait_for_termination(TERMINATION_TIMEOUT)
       end
 
       private
-
-      attr_reader :running
-
-      def running!
-        raise 'connector service is already running!' unless running.make_true
-      end
-
-      def pool
-        @pool ||= Concurrent::ThreadPoolExecutor.new(
-          min_threads: MIN_THREADS,
-          max_threads: MAX_THREADS,
-          max_queue: MAX_QUEUE,
-          fallback_policy: :abort
-        )
-      end
 
       def scheduler
         @scheduler ||= if App::Config.native_mode
@@ -83,38 +60,33 @@ module App
 
       def start_sync_task(connector_settings)
         start_heartbeat_task(connector_settings)
-        pool.post do
-          Utility::Logger.info("Starting a sync job for #{connector_settings.formatted}...")
-          Core::ElasticConnectorActions.ensure_content_index_exists(connector_settings.index_name)
-          job_runner = Core::SyncJobRunner.new(connector_settings)
-          job_runner.execute
-        rescue StandardError => e
-          Utility::ExceptionTracking.log_exception(e, "Sync job for #{connector_settings.formatted} failed due to unexpected error.")
-        end
+        Utility::Logger.info("Starting a sync job for #{connector_settings.formatted}...")
+        Core::ElasticConnectorActions.ensure_content_index_exists(connector_settings.index_name)
+        job_runner = Core::SyncJobRunner.new(connector_settings)
+        job_runner.execute
+        puts "EXECUTE DONE"
+      rescue StandardError => e
+        Utility::ExceptionTracking.log_exception(e, "Sync job for #{connector_settings.formatted} failed due to unexpected error.")
       end
 
       def start_heartbeat_task(connector_settings)
-        pool.post do
-          Utility::Logger.info("Sending heartbeat for #{connector_settings.formatted}...")
-          Core::Heartbeat.send(connector_settings)
-        rescue StandardError => e
-          Utility::ExceptionTracking.log_exception(e, "Heartbeat task for #{connector_settings.formatted} failed due to unexpected error.")
-        end
+        Utility::Logger.info("Sending heartbeat for #{connector_settings.formatted}...")
+        Core::Heartbeat.send(connector_settings)
+      rescue StandardError => e
+        Utility::ExceptionTracking.log_exception(e, "Heartbeat task for #{connector_settings.formatted} failed due to unexpected error.")
       end
 
       def start_configuration_task(connector_settings)
-        pool.post do
-          Utility::Logger.info("Updating configuration for #{connector_settings.formatted}...")
-          # when in non-native mode, populate the service type if it's not in connector settings
-          service_type = if !App::Config.native_mode && connector_settings.needs_service_type?
-                           App::Config.service_type
-                         else
-                           nil
-                         end
-          Core::Configuration.update(connector_settings, service_type)
-        rescue StandardError => e
-          Utility::ExceptionTracking.log_exception(e, "Configuration task for #{connector_settings.formatted} failed due to unexpected error.")
-        end
+        Utility::Logger.info("Updating configuration for #{connector_settings.formatted}...")
+        # when in non-native mode, populate the service type if it's not in connector settings
+        service_type = if !App::Config.native_mode && connector_settings.needs_service_type?
+                         App::Config.service_type
+                       else
+                         nil
+                       end
+        Core::Configuration.update(connector_settings, service_type)
+      rescue StandardError => e
+        Utility::ExceptionTracking.log_exception(e, "Configuration task for #{connector_settings.formatted} failed due to unexpected error.")
       end
     end
   end
