4f0a32c076a060069c690455f3b3fedb87a7d5b0
Mark Hoy <mark.hoy@elastic.co>
2023-08-29 19:44:54 -0400

    use new rbenv image

--- .buildkite/scripts/rbenv_setup.sh
+++ .buildkite/scripts/rbenv_setup.sh
@@ -2,7 +2,5 @@
 
 set -euxo pipefail
 
-curl -fsSL https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer | bash
-eval "$(~/.rbenv/bin/rbenv init - bash)"
-eval "$(~/.rbenv/bin/rbenv install $(cat .ruby-version))"
-eval "$(~/.rbenv/bin/rbenv global $(cat .ruby-version))"
+rbenv install $(cat .ruby-version)
+rbenv global $(cat .ruby-version)
