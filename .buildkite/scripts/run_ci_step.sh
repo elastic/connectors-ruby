c229b8a1119c7be03bce8780e714048a9e5498cd
Mark Hoy <mark.hoy@elastic.co>
2023-08-30 16:16:45 -0400

    re-add docker build and cleanups

--- .buildkite/scripts/run_ci_step.sh
+++ .buildkite/scripts/run_ci_step.sh
@@ -4,36 +4,25 @@ set -euxo pipefail
 
 export PATH="$PATH:/root/.rbenv/bin:/root/.rbenv/plugins/ruby-build/bin:/ci/.rbenv/shims"
 
-function install_ruby_version() {
-    local ruby_version=$(cat .ruby-version)
-    rbenv install $ruby_version
-    rbenv global $ruby_version
-}
+RUBY_VERSION=$(cat .ruby-version)
+echo "---- installing Ruby version $RUBY_VERSION"
+rbenv install $RUBY_VERSION
+rbenv global $RUBY_VERSION
 
 case $1 in
 
   tests)
-    echo "running unit tests"
-    install_ruby_version
+    echo "---- running unit tests"
     make install test
     ;;
 
   linter)
-    echo "running linter"
-    install_ruby_version
+    echo "---- running linter"
     make install lint
     ;;
 
-  docker)
-    echo "running docker build"
-    # install_ruby_version
-    # install_docker
-    make build-docker
-    ;;
-
   packaging)
-    echo "running packaging"
-    install_ruby_version
+    echo "---- running packaging"
     curl -L -o yq https://github.com/mikefarah/yq/releases/download/v4.21.1/yq_linux_amd64
     chmod +x yq
     YQ=`realpath yq` make install build_service build_service_gem
@@ -41,6 +30,7 @@ case $1 in
     ;;
 
   *)
-    echo "Usage: run_command {tests|linter|docker|packaging}"
+    echo "Usage: run_command {tests|linter|packaging}"
+    exit 2
     ;;
 esac
--- .buildkite/scripts/run_command.sh
+++ .buildkite/scripts/run_command.sh
@@ -9,27 +9,23 @@ if [[ "${COMMAND_TO_RUN:-}" == "" ]]; then
     exit 2
 fi
 
-function realpath {
-  echo "$(cd "$(dirname "$1")"; pwd)"/"$(basename "$1")";
-}
+if [[ "${COMMAND_TO_RUN:-}" == "docker" ]]; then
+  echo "running docker build"
+  make build-docker
+else
+  DOCKER_IMAGE="docker.elastic.co/ci-agent-images/enterprise-search/rbenv-buildkite-agent:latest"
+  SCRIPT_CMD="/ci/.buildkite/scripts/run_ci_step.sh"
 
-SCRIPT_WORKING_DIR=$(realpath "$(dirname "$0")")
-BUILDKITE_DIR=$(realpath "$(dirname "$SCRIPT_WORKING_DIR")")
-PROJECT_ROOT=$(realpath "$(dirname "$BUILDKITE_DIR")")
-SHARED_SCRIPT_DIR="${SCRIPT_WORKING_DIR}/shared"
-
-DOCKER_IMAGE="docker.elastic.co/ci-agent-images/enterprise-search/rbenv-buildkite-agent:latest"
-SCRIPT_CMD="/ci/.buildkite/scripts/run_ci_step.sh"
-
-docker run --interactive --rm             \
-             --sig-proxy=true --init      \
-             --user "root"                \
-             --volume "$PROJECT_ROOT:/ci" \
-             --workdir /ci                \
-             --env HOME=/ci               \
-             --env CI                     \
-             --env GIT_REVISION=${BUILDKITE_COMMIT-}        \
-             --env BUILD_ID=${BUILDKITE_BUILD_NUMBER-}      \
-             --entrypoint "${SCRIPT_CMD}" \
-             $DOCKER_IMAGE                \
-             $COMMAND_TO_RUN
+  docker run --interactive --rm             \
+              --sig-proxy=true --init      \
+              --user "root"                \
+              --volume "$PROJECT_ROOT:/ci" \
+              --workdir /ci                \
+              --env HOME=/ci               \
+              --env CI                     \
+              --env GIT_REVISION=${BUILDKITE_COMMIT-}        \
+              --env BUILD_ID=${BUILDKITE_BUILD_NUMBER-}      \
+              --entrypoint "${SCRIPT_CMD}" \
+              $DOCKER_IMAGE                \
+              $COMMAND_TO_RUN
+fi
