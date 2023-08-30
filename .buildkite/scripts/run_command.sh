0a732f74c9045956f616e26ab33a7a7c60b5199f
Mark Hoy <mark.hoy@elastic.co>
2023-08-30 12:32:10 -0400

    :facepalm: load the scripts

--- .buildkite/scripts/run_command.sh
+++ .buildkite/scripts/run_command.sh
@@ -19,9 +19,11 @@ PROJECT_ROOT=$(realpath "$(dirname "$BUILDKITE_DIR")")
 SHARED_SCRIPT_DIR="${SCRIPT_WORKING_DIR}/shared"
 
 source "${SHARED_SCRIPT_DIR}/pull_shared_scripts.sh"
+source "${SHARED_SCRIPT_DIR}/docker-retry.sh"
+source "${SHARED_SCRIPT_DIR}/run_docker_ci_script.sh"
 
 DOCKER_IMAGE="docker.elastic.co/ci-agent-images/enterprise-search/rbenv-buildkite-agent:latest"
 SCRIPT_CMD=".buildkite/scripts/run_command.sh ${COMMAND_TO_RUN}"
-ENV_VARS=("GIT_REVISION=${BUILDKITE_COMMIT-}" "BUILD_ID=${BUILDKITE_BUILD_NUMBER-}")
+ENV_VARS=("GIT_REVISION=${BUILDKITE_COMMIT-}" "BUILD_ID=${BUILDKITE_BUILD_NUMBER-}" "setuser:root")
 
 runDockerCIScript "${DOCKER_IMAGE}" "${SCRIPT_CMD}" "${ENV_VARS[@]}"
