44bbfe4ede8e0ae72cb7ede12d402c1fcc45ad43
Mark Hoy <mark.hoy@elastic.co>
2023-08-30 09:46:51 -0400

    test rbenv agent only

--- .buildkite/scripts/run_command.sh
+++ .buildkite/scripts/run_command.sh
@@ -4,30 +4,60 @@ set -euxo pipefail
 
 export PATH="$PATH:~/.rbenv/bin"
 
-rbenv install $(cat .ruby-version)
-rbenv global $(cat .ruby-version)
+function install_ruby_version() {
+    local ruby_version=$(cat .ruby-version)
+    rbenv install $ruby_version
+    rbenv global $ruby_version
+}
 
-COMMAND_TO_RUN=${1-}
+function install_docker() {
+    # apt-cache madison docker-ce | awk '{ print $3 }'
 
-case COMMAND_TO_RUN in
+    # curl -fsSL https://get.docker.com | sh
+    #apt update && apt install docker.io -y
+
+    #apt-get update
+    #apt-get install -y gnupg
+    #install -m 0755 -d /etc/apt/keyrings
+    #curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    #chmod a+r /etc/apt/keyrings/docker.gpg#
+
+    #echo \
+    #    "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+    #    "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+    #    tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+    #apt-get update
+    #apt-get install -y \
+    #    docker-ce docker-ce-cli containerd.io \
+    #    docker-buildx-plugin docker-compose-plugin
+    #service docker start
+}
+
+case $1 in
 
   tests)
     echo "running unit tests"
+    install_ruby_version
     make install test
     ;;
 
   linter)
     echo "running linter"
+    install_ruby_version
     make install lint
     ;;
 
   docker)
     echo "running docker build"
+    # install_ruby_version
+    install_docker
     make build-docker
     ;;
 
   packaging)
     echo "running packaging"
+    install_ruby_version
     curl -L -o yq https://github.com/mikefarah/yq/releases/download/v4.21.1/yq_linux_amd64
     chmod +x yq
     YQ=`realpath yq` make install build_service build_service_gem
