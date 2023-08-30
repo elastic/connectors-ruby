1cbc4b4348e88b24caa96941308b2462d233d715
Mark Hoy <mark.hoy@elastic.co>
2023-08-30 12:06:12 -0400

    try using a generic VM with rbenv setup

--- .buildkite/scripts/run_command.sh
+++ .buildkite/scripts/run_command.sh
@@ -6,33 +6,11 @@ export PATH="$PATH:~/.rbenv/bin"
 
 function install_ruby_version() {
     local ruby_version=$(cat .ruby-version)
+    curl -fsSL https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer | bash
+    echo 'eval "$(~/.rbenv/bin/rbenv init - bash)"' >> ~/.bashrc
     rbenv install $ruby_version
     rbenv global $ruby_version
 }
-
-# function install_docker() {
-    # apt-cache madison docker-ce | awk '{ print $3 }'
-
-    # curl -fsSL https://get.docker.com | sh
-    #apt update && apt install docker.io -y
-
-    #apt-get update
-    #apt-get install -y gnupg
-    #install -m 0755 -d /etc/apt/keyrings
-    #curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-    #chmod a+r /etc/apt/keyrings/docker.gpg#
-
-    #echo \
-    #    "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-    #    "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-    #    tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-    #apt-get update
-    #apt-get install -y \
-    #    docker-ce docker-ce-cli containerd.io \
-    #    docker-buildx-plugin docker-compose-plugin
-    #service docker start
-# }
 
 case $1 in
 
